16e8f8b0c21dcd72670195eb1330c0b86346fbab
snowch <chsnow123@gmail.com>
2026-01-22 15:29:39 +0000

    passwordless jupyter

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-docker run -it -p 8888:8888 -v "${PWD}":/home/jovyan/work quay.io/jupyter/pytorch-notebook
+docker run -it -p 8888:8888 \
+	-v "${PWD}":/home/jovyan/work quay.io/jupyter/pytorch-notebook \
+	start-notebook.py --IdentityProvider.token=''
+
